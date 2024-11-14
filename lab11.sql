fc863749516b422a8b25ec213360c92bdff0f22c
seanklee <34324485+seanklee@users.noreply.github.com>
2024-11-14 10:33:09 -0500

    Update lab11.sql

--- lab11.sql
+++ lab11.sql
@@ -1,1 +1,29 @@
+--comments
+SELECT * 
+FROM courses
 
+SELECT * FROM courses;
+
+SELECT * FROM assignments
+LIMIT 10; 
+
+SELECT count(*) FROM courses;
+
+SELECT min(due_date) FROM assignments;
+
+SELECT *
+FROM  courses
+WHERE course_name LIKE 'Intro%';
+
+SELECT *
+FROM  assignments
+WHERE status != 'Completed'
+ORDER BY due_date;
+
+
+SELECT course_id, title, status, due_date
+FROM assignments
+WHERE status != 'Completed'	
+  AND course_id LIKE 'COMM%'
+  AND due_date < '2024-12-31'
+ORDER BY due_date;
